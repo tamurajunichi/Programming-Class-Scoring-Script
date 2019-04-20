e3b0a515af374d5277ac77ba7576dce0489d9ea4
Tamura_Junichi <r201603849je@jindai.jp>
2019-04-21 00:59:45 +0900

    add the program kadai_check.py . this python file is using a python standard libraly "subprocess" and im still working on that....

---
+++ kadai_check.py
@@ -0,0 +1,15 @@
+import subprocess
+
+
+class Command():
+    def __init__(self, command):
+        self.arg = command.split()
+
+    def proc(self):
+        with subprocess.Popen(self.arg, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True) as proc:
+            message = proc.stdout.read()
+            print(message.decode("cp932"))
+
+
+gccv = Command("gcc -v")
+gccv.proc()
