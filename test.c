bd2b1b4f951dd41ea93a20b7a7b90fee5e8debba
tamurajunichi <r201603849je@jindai.jp>
2019-04-21 19:32:09 +0900

    untracked files on master: 15624e7 kadai

---
+++ test.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+	int x;
+	print
+	scanf("%d",&x);
+	printf("test1:%d",x);
+	scanf("%d",&x);
+	printf("test2:%d",x);
+	scanf("%d",&x);
+	printf("test3:%d",x);
+	return 0;
+}
+
+
+
